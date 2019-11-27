4f7c91a2d8709109d3d1f7598b5607cca67cea90
Reese M Bowman <reesemb1@gmail.com>
2019-11-27 04:29:22 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,10 @@ first_number = 7
 second_number = 4
 
 sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
+
